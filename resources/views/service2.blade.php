c2d5e4b78fecd05be7ac54e607dcf6692676b0be
Rusel T. Tayong <ruseltayong@gmail.com>
2024-01-05 20:02:17 +0800

    PushUpdates

--- resources/views/service2.blade.php
+++ resources/views/service2.blade.php
@@ -214,138 +214,85 @@
                     </ul>
                 </div>
             </div>
-            <div class="-mx-4 flex flex-wrap"  x-data='{ modalShow: false, services: @json($services), capitalizeFirstChar: function(str) { return str.charAt(0).toUpperCase() + str.slice(1); } }'>
+            <div class="-mx-4 flex flex-wrap" x-data='{ modalShow: false, services: @json($services), capitalizeFirstChar: function(str) { return str.charAt(0).toUpperCase() + str.slice(1); } }'>
                 <template x-for="service in services" :key="service.id">
                     <div x-show="showCards === 'all' || showCards === service.category" class="w-full px-4 md:w-1/2 xl:w-1/3">
                         <div class="relative mb-12">
                             <div class="overflow-hidden rounded-lg">
-                            <img
-                                x-bind:src="'fileupload/services/'+service.picture"
-                                alt="portfolio"
-                                class="w-full"
-                            />
+                                <img
+                                    x-bind:src="'fileupload/services/'+service.picture"
+                                    alt="portfolio"
+                                    class="w-full"
+                                />
                             </div>
-                            <div
-                            class="relative z-10 mx-7 -mt-20 rounded-lg bg-white py-9 px-3 text-center shadow-lg"
-                            >
-                            <span class="mb-2 block text-sm font-semibold text-[#011523]" x-text="capitalizeFirstChar(service.category)"></span>
-                            <h3 class="mb-4 text-xl font-bold text-dark" x-text="service.title"></h3>
-                            <a
-                                href="javascript:void(0)"
-                                class="inline-block rounded-md border py-3 px-7 text-sm font-semibold text-body-color transition hover:border-[#011523] hover:bg-[#011523] hover:text-white"
-                            >
-                                View Details
-                            </a>
+                            <div class="relative z-10 mx-7 -mt-20 rounded-lg bg-white py-9 px-3 text-center shadow-lg">
+                                <span class="mb-2 block text-sm font-semibold text-[#011523]" x-text="capitalizeFirstChar(service.category)"></span>
+                                <h3 class="mb-4 text-xl font-bold text-dark" x-text="service.title"></h3>
+                                <section x-data="{modalOpen: false}">
+                                    <button type="button" @click="modalOpen = true" class="inline-block rounded-md border py-3 px-7 text-sm font-semibold text-body-color transition hover:border-[#011523] hover:bg-[#011523] hover:text-white">
+                                        View Details
+                                    </button>
+                                    <div x-show="modalOpen" class="fixed top-0 left-0 flex items-center justify-center w-full h-screen py-10 overflow-y-scroll dark:bg-dark" >
+                                        <div @click.outside="modalOpen = false" class="mx-auto max-w-[560px] rounded-[10px] bg-white dark:bg-dark-2 p-8 shadow-1 dark:shadow-3">
+                                            <h3 class="flex mb-5 text-lg font-semibold text-dark dark:text-white sm:text-xl">
+                                                <span class="inline-block pr-[10px]">
+                                                    <svg
+                                                    width="24"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    >
+                                                    <g clip-path="url(#clip0_2095_7581)">
+                                                        <path
+                                                            d="M12 0.674988C5.73749 0.674988 0.674988 5.73749 0.674988 12C0.674988 18.2625 5.73749 23.3625 12 23.3625C18.2625 23.3625 23.3625 18.2625 23.3625 12C23.3625 5.73749 18.2625 0.674988 12 0.674988ZM12 21.675C6.67499 21.675 2.36249 17.325 2.36249 12C2.36249 6.67499 6.67499 2.36249 12 2.36249C17.325 2.36249 21.675 6.71249 21.675 12.0375C21.675 17.325 17.325 21.675 12 21.675Z"
+                                                            fill="#F27430"
+                                                            />
+                                                        <path
+                                                            d="M13.5 10.2H10.5C10.05 10.2 9.63751 10.575 9.63751 11.0625V18.5625C9.63751 19.0125 10.0125 19.425 10.5 19.425H13.5C13.95 19.425 14.3625 19.05 14.3625 18.5625V11.0625C14.3625 10.575 13.95 10.2 13.5 10.2ZM12.675 17.7H11.3625V11.8875H12.675V17.7Z"
+                                                            fill="#F27430"
+                                                            />
+                                                        <path
+                                                            d="M12 4.61249C10.725 4.61249 9.63751 5.66249 9.63751 6.97499C9.63751 8.28749 10.6875 9.33749 12 9.33749C13.3125 9.33749 14.3625 8.28749 14.3625 6.97499C14.3625 5.66249 13.275 4.61249 12 4.61249ZM12 7.61249C11.625 7.61249 11.325 7.31249 11.325 6.93749C11.325 6.56249 11.625 6.26249 12 6.26249C12.375 6.26249 12.675 6.56249 12.675 6.93749C12.675 7.31249 12.375 7.61249 12 7.61249Z"
+                                                            fill="#F27430"
+                                                            />
+                                                    </g>
+                                                    <defs>
+                                                        <clipPath id="clip0_2095_7581">
+                                                            <rect width="24" height="24" fill="white" />
+                                                        </clipPath>
+                                                    </defs>
+                                                    </svg>
+                                                </span>
+                                                Are you sure you want to submit?
+                                            </h3>
+                                            <p class="text-sm mb-7 dark:text-dark-6 text-body-color">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis
+                                                bibendum purus. Aliquam nec ipsum commodo ligula tristique
+                                                scelerisque sed sed enim. Suspendisse varius libero just.
+                                            </p>
+                                            <div class="flex items-center justify-end space-x-1">
+                                                <button
+                                                    @click="modalOpen = false"
+                                                    class="px-5 py-2 text-sm font-medium bg-white rounded-md text-dark shadow-1 dark:shadow-3 hover:text-white hover:bg-red-dark dark:text-white dark:bg-white/5"
+                                                    >
+                                                Cancel
+                                                </button>
+                                                <button
+                                                    @click="modalOpen = false"
+                                                    class="px-5 py-2 text-sm font-medium text-white rounded-md bg-primary hover:bg-opacity-90"
+                                                    >
+                                                Submit
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
                             </div>
                         </div>
                     </div>
                 </template>
-                {{-- <div :class="showCards == 'all' || showCards == 'marketing' ? 'block' : 'hidden'" class="w-full px-4 md:w-1/2 xl:w-1/3">
-                    <div class="relative mb-12">
-                        <div class="overflow-hidden rounded-lg">
-                            <img src="{{ asset('/images/portfolio/portfolio-01/image-02.jpg') }}" alt="portfolio"
-                                class="w-full" />
-                        </div>
-                        <div class="relative z-10 mx-7 -mt-20 rounded-lg bg-white py-9 px-3 text-center shadow-lg">
-                            <span class="mb-2 block text-sm font-semibold text-[#011523]">
-                                Marketing
-                            </span>
-                            <h3 class="mb-4 text-xl font-bold text-dark">
-                                Best Marketing tips
-                            </h3>
-                            <a href="javascript:void(0)"
-                                class="inline-block rounded-md border py-3 px-7 text-sm font-semibold text-body-color transition hover:border-[#011523] hover:bg-[#011523] hover:text-white">
-                                View Details
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div :class="showCards == 'all' || showCards == 'development' ? 'block' : 'hidden'"
-                    class="w-full px-4 md:w-1/2 xl:w-1/3">
-                    <div class="relative mb-12">
-                        <div class="overflow-hidden rounded-lg">
-                            <img src="{{ asset('/images/portfolio/portfolio-01/image-03.jpg') }}" alt="portfolio"
-                                class="w-full" />
-                        </div>
-                        <div class="relative z-10 mx-7 -mt-20 rounded-lg bg-white py-9 px-3 text-center shadow-lg">
-                            <span class="mb-2 block text-sm font-semibold text-[#011523]">
-                                Development
-                            </span>
-                            <h3 class="mb-4 text-xl font-bold text-dark">
-                                Web Design Trend
-                            </h3>
-                            <a href="javascript:void(0)"
-                                class="inline-block rounded-md border py-3 px-7 text-sm font-semibold text-body-color transition hover:border-[#011523] hover:bg-[#011523] hover:text-white">
-                                View Details
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div :class="showCards == 'all' || showCards == 'design' ? 'block' : 'hidden'"
-                    class="w-full px-4 md:w-1/2 xl:w-1/3">
-                    <div class="relative mb-12">
-                        <div class="overflow-hidden rounded-lg">
-                            <img src="{{ asset('/images/portfolio/portfolio-01/image-04.jpg') }}" alt="portfolio"
-                                class="w-full" />
-                        </div>
-                        <div class="relative z-10 mx-7 -mt-20 rounded-lg bg-white py-9 px-3 text-center shadow-lg">
-                            <span class="mb-2 block text-sm font-semibold text-[#011523]">
-                                Design
-                            </span>
-                            <h3 class="mb-4 text-xl font-bold text-dark">
-                                Business Card Design
-                            </h3>
-                            <a href="javascript:void(0)"
-                                class="inline-block rounded-md border py-3 px-7 text-sm font-semibold text-body-color transition hover:border-[#011523] hover:bg-[#011523] hover:text-white">
-                                View Details
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div :class="showCards == 'all' || showCards == 'marketing' ? 'block' : 'hidden'"
-                    class="w-full px-4 md:w-1/2 xl:w-1/3">
-                    <div class="relative mb-12">
-                        <div class="overflow-hidden rounded-lg">
-                            <img src="{{ asset('/images/portfolio/portfolio-01/image-05.jpg') }}" alt="portfolio"
-                                class="w-full" />
-                        </div>
-                        <div class="relative z-10 mx-7 -mt-20 rounded-lg bg-white py-9 px-3 text-center shadow-lg">
-                            <span class="mb-2 block text-sm font-semibold text-[#011523]">
-                                Marketing
-                            </span>
-                            <h3 class="mb-4 text-xl font-bold text-dark">
-                                Digital marketing
-                            </h3>
-                            <a href="javascript:void(0)"
-                                class="inline-block rounded-md border py-3 px-7 text-sm font-semibold text-body-color transition hover:border-[#011523] hover:bg-[#011523] hover:text-white">
-                                View Details
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div :class="showCards == 'all' || showCards == 'branding' ? 'block' : 'hidden'"
-                    class="w-full px-4 md:w-1/2 xl:w-1/3">
-                    <div class="relative mb-12">
-                        <div class="overflow-hidden rounded-lg">
-                            <img src="{{ asset('/images/portfolio/portfolio-01/image-06.jpg') }}" alt="portfolio"
-                                class="w-full" />
-                        </div>
-                        <div class="relative z-10 mx-7 -mt-20 rounded-lg bg-white py-9 px-3 text-center shadow-lg">
-                            <span class="mb-2 block text-sm font-semibold text-[#011523]">
-                                Branding
-                            </span>
-                            <h3 class="mb-4 text-xl font-bold text-dark">
-                                Creative Agency
-                            </h3>
-                            <a href="javascript:void(0)"
-                                class="inline-block rounded-md border py-3 px-7 text-sm font-semibold text-body-color transition hover:border-[#011523] hover:bg-[#011523] hover:text-white">
-                                View Details
-                            </a>
-                        </div>
-                    </div>
-                </div> --}}
             </div>
-            
             <div class="text-center">
                <div
                   class="inline-flex p-3 mb-12 bg-white rounded dark:bg-dark-2 shadow-1 dark:shadow-3"
@@ -434,36 +381,36 @@
     </div>
 </section>
 <section
-class="relative z-10 newsletter-bg"
-x-data="{ isMobile: window.innerWidth <= 600 }"
-x-init="() => {
-    window.addEventListener('resize', () => {
-        isMobile = window.innerWidth <= 600;
-        console.log(window.innerWidth);
-    });
-}"
->
-<div class="container mx-auto">
-   <div class="text-center lg:pt-[20px]"
-   :style="isMobile ? 'padding-top: 30px;' : ''">
+    class="relative z-10 newsletter-bg"
+    x-data="{ isMobile: window.innerWidth <= 600 }"
+    x-init="() => {
+        window.addEventListener('resize', () => {
+            isMobile = window.innerWidth <= 600;
+            console.log(window.innerWidth);
+        });
+    }"
+    >
+    <div class="container mx-auto">
+        <div class="text-center lg:pt-[20px]"
+        :style="isMobile ? 'padding-top: 30px;' : ''">
 
-      <h2 class="text-3xl text-white font-semibold opacity-0 join" 
-          id="join"
-          :style="isMobile ? 'font-size: 22px' : ''"
-         >Join Our Newsletter Now
-      </h2>
+            <h2 class="text-3xl text-white font-semibold opacity-0 join" 
+                id="join"
+                :style="isMobile ? 'font-size: 22px' : ''"
+                >Join Our Newsletter Now
+            </h2>
 
-      <form action="#" method="post" class="mt-4 space-x-2 opacity-0 pb-3" id="signUp">
-          <input
-          type="text"
-          placeholder="Enter your Email"
-          class="w-1/4 px-5 py-3 text-base bg-white border outline-none border-stroke text-body-color dark:text-white dark:border-dark-3"
-          :style="isMobile ? 'width: 200px' : ''"/>
-          <button type="submit" class="bg-white text-dark px-5 py-3 font-semibold">SIGN UP</button>
-      </form>
-      <p class="text-white opacity-90 pb-10" id="subscribe">You can unsubscribe anytime</p>
-  </div>
-</div>
+            <form action="#" method="post" class="mt-4 space-x-2 opacity-0 pb-3" id="signUp">
+                <input
+                type="text"
+                placeholder="Enter your Email"
+                class="w-1/4 px-5 py-3 text-base bg-white border outline-none border-stroke text-body-color dark:text-white dark:border-dark-3"
+                :style="isMobile ? 'width: 200px' : ''"/>
+                <button type="submit" class="bg-white text-dark px-5 py-3 font-semibold">SIGN UP</button>
+            </form>
+            <p class="text-white opacity-90 pb-10" id="subscribe">You can unsubscribe anytime</p>
+        </div>
+    </div>
 </section>
 
 <footer class="relative z-10 bg-white dark:bg-dark lg:pt-[120px]"
