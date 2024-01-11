6db9824125249c7c5dc52f330055c29b44bd5f06
Rodfil T. Tayong <rttayong@cli.lan>
2024-01-11 13:34:04 +0800

    added darkmode in view details services

--- resources/views/service2.blade.php
+++ resources/views/service2.blade.php
@@ -147,7 +147,7 @@
                 <div x-show="modalOpen" class="bg-[#000000] bg-opacity-20 fixed z-50 top-0 left-0 flex items-center justify-center w-full dark:bg-dark h-screen overflow-y-scroll">
                     <div @click.outside="modalOpen = false" class="relative mx-auto max-w-[660px]
                     bg-white dark:bg-dark-2 shadow-1 dark:shadow-3 h-[95vh]">
-                        <div class="p-8 bg-white rounded-[10px] ">
+                        <div class="p-8 bg-white dark:bg-dark-2 rounded-[10px] ">
                             <button @click="modalOpen = false" class="bg-red p-1 absolute right-5 top-5 rounded-[5px] text-white dark:text-dark-6">
                                 <svg 
                                     width="20" 
@@ -155,7 +155,7 @@
                                     viewBox="0 0 20 20" 
                                     fill="none" 
                                     xmlns="http://www.w3.org/2000/svg"
-                                    class="fill-current"
+                                    class="fill-current dark:fill-white"
                                     >
                                     <path 
                                     d="M11 10L18.625 2.375C18.9062 2.09375 18.9062 1.65625 18.625 1.375C18.3438 1.09375 17.9063 1.09375 17.625 1.375L10 9L2.375 1.375C2.09375 1.09375 1.65625 1.09375 1.375 1.375C1.09375 1.65625 1.09375 2.09375 1.375 2.375L9 10L1.375 17.625C1.09375 17.9063 1.09375 18.3438 1.375 18.625C1.5 18.75 1.6875 18.8438 1.875 18.8438C2.0625 18.8438 2.25 18.7812 2.375 18.625L10 11L17.625 18.625C17.75 18.75 17.9375 18.8438 18.125 18.8438C18.3125 18.8438 18.5 18.7812 18.625 18.625C18.9062 18.3438 18.9062 17.9063 18.625 17.625L11 10Z" 
