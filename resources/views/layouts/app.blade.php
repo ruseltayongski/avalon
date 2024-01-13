2cc4b69e30d7dacb5fc9d403dffc340d2d61caa8
Rodfil Tayong <trodfil@gmail.com>
2024-01-13 13:03:43 +0800

    PushUpdates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--     <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+    
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Avalon') }}</title>
+    <link rel="shortcut icon" type="x-icon" href="{{ asset('/images/avalon-logo.png') }}">
+
+
+    <title>{{ config('app.name', 'Avalon House') }}</title>
 
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
