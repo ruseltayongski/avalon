3eb2d2379909e76e3ff609ec13f2e89b85996495
Rusel T. Tayong <ruseltayong@gmail.com>
2024-01-25 00:09:45 +0800

    added promo code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ServicesSeeder::class,
             FinanceServicesSeeder::class,
+            PromoCodeSeeder::class,
         ]);
     }
 }
