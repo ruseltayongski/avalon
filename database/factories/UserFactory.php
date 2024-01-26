3c43f867d1d2930609da81fbd6bcaaf1399455b0
Rodfil Tayong <trodfil@gmail.com>
2024-01-27 01:58:23 +0800

    PushUpdates

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => "user@gmail.com",
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+        /*     'password' => static::$password ??= Hash::make('password'), */
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -16,32 +17,37 @@ class UserSeeder extends Seeder
         User::factory()
          ->count(1)
          ->state([
-            'email' => 'admin1@gmail.com'
+            'email' => 'admin1@gmail.com',
+            'password' => Hash::make('password1')
          ])
         ->create();
         User::factory()
          ->count(1)
          ->state([
-            'email' => 'admin2@gmail.com'
+            'email' => 'admin2@gmail.com',
+            'password' => Hash::make('password2')
          ])
-        ->create();
-        User::factory()
+         ->create();
+         User::factory()
          ->count(1)
          ->state([
-            'email' => 'admin3@gmail.com'
+            'email' => 'admin3@gmail.com',
+            'password' => Hash::make('password3')
          ])
-        ->create();
-        User::factory()
+         ->create();
+         User::factory()
          ->count(1)
          ->state([
-            'email' => 'admin4@gmail.com'
+            'email' => 'admin4@gmail.com',
+            'password' => Hash::make('password4')
          ])
-        ->create();
-        User::factory()
+         ->create();
+         User::factory()
          ->count(1)
          ->state([
-            'email' => 'admin5@gmail.com'
+            'email' => 'admin5@gmail.com',
+            'password' => Hash::make('password5')
          ])
-        ->create();
+         ->create();
     }
 }
