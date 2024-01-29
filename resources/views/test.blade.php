2c53148cb2052e878dccbe44c700cbbbc6b97c3d
Rusel T. Tayong <ruseltayong@gmail.com>
2024-01-29 16:51:33 +0800

    PusHUpdates

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,12 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Open Folder</title>
+    <title>Download Files</title>
 </head>
 <body>
-    <form method="GET">
-        <a href="{{ asset('test1') }}" target="_blank">Open Folder</a>
-    </form>
+    <?php
+    $folderPath = 'C:\xampp_8\htdocs';
+    ?>
+
+    <p>Click the button to download files from the folder:</p>
+    
+    <?php
+    // Get a list of files in the folder
+    $files = scandir($folderPath);
+    
+    // Exclude '.' and '..' from the list
+    $files = array_diff($files, array('.', '..'));
+    
+    // Display links for each file
+    foreach ($files as $file) {
+        $filePath = $folderPath . DIRECTORY_SEPARATOR . $file;
+        echo "<a href=\"download.php?file=" . urlencode($filePath) . "\" download>" . $file . "</a><br>";
+    }
+    ?>
+
 </body>
 </html>
-
